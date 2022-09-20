ea8f48880773edc1f4d8dcbd1d58795d1083ef81
kilinsik <42113574+tapoo5602@users.noreply.github.com>
2022-09-20 16:30:06 +0900

    insik 0916
    1

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import random
 import hashlib
 import datetime
 import jwt
-import sys
 
 app = Flask(__name__)
 client = MongoClient('localhost', 27017)
@@ -42,7 +41,7 @@ def api_login():
     if result is not None:
         payload = {
             'id' : first_name_receive,
-            'exp' : datetime.datetime.utcnow() + datetime.timedelta(seconds=60)
+            'exp' : datetime.datetime.utcnow() + datetime.timedelta(seconds=5)
         }
         token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
 
@@ -56,13 +55,16 @@ def api_login():
 def home():
     token_receive = request.cookies.get('mytoken')
     try:
+        print(token_receive)
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
         user_info = db.user.find_one({"id": payload['id']})
-        return render_template('main.html', nickname=user_info["nick"])
+        return render_template("main.html")
     except jwt.ExpiredSignatureError:
-        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+        return redirect(url_for("index", msg="로그인 시간이 만료되었습니다."))
     except jwt.exceptions.DecodeError:
-        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
+        return redirect(url_for("index", msg="로그인 정보가 존재하지 않습니다."))
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
